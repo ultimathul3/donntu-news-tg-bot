c33b692c1f5dde9f4ae4b35e1d70f32c30298bc5
root <root@vm456797.u150944.kvm.ubuntu.20.04.64>
2022-10-31 16:55:47 +0300

    Add secret token check

--- main.go
+++ main.go
@@ -20,6 +20,11 @@ var (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("X-Telegram-Bot-Api-Secret-Token") != os.Getenv("SECRET_TOKEN") {
+		fileLog.Info("invalid secret token from request")
+		return
+	}
+
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		fileLog.Info("body read error:", err)
